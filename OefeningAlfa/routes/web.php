0d3329c38b0276a3f3551557419f52497949eeda
alfabrand <joostjasper@live.nl>
2018-09-06 14:46:05 +0200

    cmd
    joost

--- OefeningAlfa/routes/web.php
+++ OefeningAlfa/routes/web.php
@@ -1,4 +1,14 @@
 <?php
 
 
+<<<<<<< Updated upstream
 Route::get('/', 'PathController@homepage');
+=======
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/master', function () {
+    return view('resources.views.webshop.master');
+});
+>>>>>>> Stashed changes
