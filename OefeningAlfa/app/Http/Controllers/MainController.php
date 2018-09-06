d139d9ed2a18653127d8dc537cbd8b23f0a1ded6
Unknown <Joriscleven@hotmail.nl>
2018-09-06 14:37:49 +0200

    update controllers
    -Joris

--- OefeningAlfa/app/Http/Controllers/MainController.php
+++ OefeningAlfa/app/Http/Controllers/MainController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class CommentsController extends Controller
+class MainController extends Controller
 {
+  public function homepage()
+{
+  return view('layouts.master');
+}
 
 }
