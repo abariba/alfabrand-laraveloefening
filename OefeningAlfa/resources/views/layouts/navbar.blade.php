4e64ae681a78165c3be878ba3155d74684110d99
Sha're Schoenaker <shareschoenaker@gmail.com>
2018-09-07 14:35:08 +0200

    navbar aangepast

--- OefeningAlfa/resources/views/layouts/navbar.blade.php
+++ OefeningAlfa/resources/views/layouts/navbar.blade.php
@@ -3,17 +3,17 @@
       <a class="py-2" href="#">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
       </a>
-      <a class="py-2 d-none d-md-inline-block" href="/joost">Joost</a>
-      <a class="py-2 d-none d-md-inline-block" href="/joris">Joris</a>
-      <a class="py-2 d-none d-md-inline-block" href="/jessy">Jessy</a>
+      <li class="nav-item"><a class="nav-link py-2 d-none d-md-inline-block" href="/joost">Joost</a></li>
+      <li class="nav-item"><a class="nav-link py-2 d-none d-md-inline-block" href="/joris">Joris</a></li>
+      <li class="nav-item"><a class="nav-link py-2 d-none d-md-inline-block" href="/jessy">Jessy</a></li>
       <ul class="navbar-nav ml-auto">
           <!-- Authentication Links -->
           @guest
               <li class="nav-item">
-                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                  <a class="nav-link py-2 d-none d-md-inline-block" href="{{ route('login') }}">{{ __('Login') }}</a>
               </li>
               <li class="nav-item">
-                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                  <a class="nav-link py-2 d-none d-md-inline-block" href="{{ route('register') }}">{{ __('Register') }}</a>
               </li>
           @else
               <li class="nav-item dropdown">
