5160cf66a3ee2e9555b7c09990ae99ec8eeb4bd7
Sha're Schoenaker <shareschoenaker@gmail.com>
2018-09-11 12:09:11 +0200

    nav change

--- OefeningAlfa/resources/views/layouts/navbar.blade.php
+++ OefeningAlfa/resources/views/layouts/navbar.blade.php
@@ -10,7 +10,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto text-center">
                 <li class="nav-item">
-                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="http://127.0.0.1:8000">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/joost">Joost</a>
