fc8639bcc13b0a90c9b6fa280da28a1b0f1bb2cf
Sha're Schoenaker <shareschoenaker@gmail.com>
2018-09-12 15:00:04 +0200

    Img home

--- OefeningAlfa/resources/views/pages/index.blade.php
+++ OefeningAlfa/resources/views/pages/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
+<img src="/img/foto.jpeg" alt="Smiley face" height="42" width="42">
    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
         <div class="col-md-5 p-lg-5 mx-auto my-5">
           <h1 class="display-4 font-weight-normal">Punny headline</h1>
