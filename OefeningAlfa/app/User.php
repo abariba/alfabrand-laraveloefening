6880d318090d90a781627580c7c4890b19aeea15
alfabrand <joostjasper@live.nl>
2018-09-18 11:08:55 +0200

    fillable name

--- OefeningAlfa/app/User.php
+++ OefeningAlfa/app/User.php
@@ -2,9 +2,24 @@
 namespace App;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 class User extends Model implements Authenticatable
 {
     use \Illuminate\Auth\Authenticatable;
+    use Notifiable;
+    protected $fillable = [
+        'name', 'email', 'password','level',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
     public function comments()
     {
         return $this->hasMany('App\comments');
